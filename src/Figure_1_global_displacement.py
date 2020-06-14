35f4f8cabec0447f28d6a98c56c6ba526653bcdf
rik linssen <rik.linssen@oxfamnovib.nl>
2020-06-14 13:20:47 +0200

    fig 1 1st version

---
+++ src/Figure_1_global_displacement.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import seaborn as sns
+
+root=Path.cwd()
+dta=root/"data/dta"
+
+
+glob_overview=pd.read_stata(dta/"global overview.dta")
+
+data=glob_overview.loc[:, ['Year', 'Refugees', 'IDPs', 'Totaldisplaced', 'Worldpop']]
+#lowercase
+data.columns=[c.lower() for c in data.columns]
+#make a datetime index
+data['yr']=data['year'].apply(lambda x: '1/1/'+ str(x))
+data['year']=pd.to_datetime(data['yr'], infer_datetime_format=True)
+data.set_index('year', inplace=True)
+data.index=pd.to_datetime(data.index, format='%Y').year
+# to numeric exclude missings 
+for c in ['idps', 'totaldisplaced']: 
+    data[c]=pd.to_numeric(data[c], errors='coerce')
+
+#drop redundant
+data=data.drop(columns='yr')
+
+#in millions
+data=data/1000000
+
+#in percentages
+data['p_refugees']=data['refugees']/data['worldpop']
+data['p_idps']=data['idps']/data['worldpop']
+data['p_totaldisplaced']=data['totaldisplaced']/data['worldpop']
+
+
+
+#figure 1 Global displacement: 1951 - 2018 (absolute numbers, in millions)
+#colormap photocopy safe
+colors=['#e41a1c','#377eb8','#4daf4a']
+labels=["Total displaced", "IDP's", "Refugees"]
+
+sns.set_context('paper')
+plt.style.use('seaborn-ticks')
+
+totallist=['totaldisplaced','idps', 'refugees' ]
+fig, (ax1, ax2)=plt.subplots(nrows=2, ncols=1, sharex='col')
+#total displaced
+ax1.plot(data.index, data['totaldisplaced'], color='#e41a1c', linestyle='solid')
+#idps
+ax1.plot(data.index, data['idps'], color='#377eb8', linestyle='dashed')
+#refugees
+ax1.plot(data.index, data['refugees'], color='#4daf4a', linestyle='dotted')
+ax1.set_title("Absolute numbers, in millions")
+#bootm axes
+#total displaced
+ax2.plot(data.index, data['p_totaldisplaced'], color='#e41a1c', linestyle='dashed')
+#idps
+ax2.plot(data.index, data['p_idps'], color='#377eb8', linestyle='dashed')
+#refugees
+ax2.plot(data.index, data['p_refugees'], color='#bebada', linestyle='dashed')
+ax2.set_title("Relative to the world population (%): 1951-2018")
+
+
+#annotations/legend
+maxyear=data.index.max()
+
+
+for (c, kleur, lab) in zip(['totaldisplaced', 'idps','refugees'], colors, labels): 
+    ax1.text(x=maxyear+1.5, y=data.at[maxyear,c], s=lab, color=kleur)
+for (c, kleur, lab) in zip(['p_totaldisplaced', 'p_idps','p_refugees'], colors, labels): 
+    ax2.text(x=maxyear+1.5, y=data.at[maxyear,c], s="% "+lab, color=kleur)
+
+#y-axis
+ax2.
+
+for ax in fig.axes: 
+    ax.grid(which='major', axis='y', linestyle=':')
+sns.despine()
+
+fig.suptitle('Global displacement: 1951 - 2018')
+#export=glob_overview.to_csv('test.csv')
